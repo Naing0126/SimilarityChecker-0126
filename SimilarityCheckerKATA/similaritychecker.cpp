ddea4cdc39bf7fe2a860fc98aca5f32d7aba6c29
naing.yoon <naing.yoon@samsung.com>
2025-07-23 15:31:45 +0900

    [Feature] Add Zero Point case

--- SimilarityCheckerKATA/similaritychecker.cpp
+++ SimilarityCheckerKATA/similaritychecker.cpp
@@ -5,6 +5,9 @@ class LengthChecker {
 public:
 	int GetLengthPoint(string str1, string str2) {
 		if (str1.length() == str2.length()) return MAX_POINT;
+		
+		if (str1.length() >= str2.length() * 2) return 0;
+		if (str1.length() * 2 <= str2.length()) return 0;
 	}
 private:
 	const int MAX_POINT = 60;
--- SimilarityCheckerKATA/similaritychecker_test.cpp
+++ SimilarityCheckerKATA/similaritychecker_test.cpp
@@ -4,8 +4,11 @@
 
 TEST(LengthCheck, SameSize) {
 	LengthChecker sc;
-	// 글자수검사
 	EXPECT_EQ(60, sc.GetLengthPoint("ASD", "DSA"));
+}
+TEST(LengthCheck, Zero) {
+	LengthChecker sc;
+	EXPECT_EQ(0, sc.GetLengthPoint("A", "BB"));
 }
 int main() {
 	::testing::InitGoogleMock();
