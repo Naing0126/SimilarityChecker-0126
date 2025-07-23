3e181466ff8a4f02c4f8dba562db504a37c81398
naing.yoon <naing.yoon@samsung.com>
2025-07-23 15:40:43 +0900

    [Feature] Add Partial Points case

--- SimilarityCheckerKATA/similaritychecker.cpp
+++ SimilarityCheckerKATA/similaritychecker.cpp
@@ -7,6 +7,14 @@ public:
 		if (str1.length() == str2.length()) return MAX_POINT;
 		
 		if (IsBiggerThanTwice(str1, str2)) return ZERO_POINT;
+
+		int gap = 0;
+		if (str1.length() > str2.length())
+			gap = str1.length() - str2.length();
+		else
+			gap = str2.length() - str1.length();
+		double partial = (1 - (static_cast<double>(gap) / 3)) * 60;
+		return partial;
 	}
 private:
 	bool IsBiggerThanTwice(std::string& str1, std::string& str2)
@@ -15,6 +23,7 @@ private:
 		if (str2.length() >= str1.length() * 2) return true;
 		return false;
 	}
+
 	const int MAX_POINT = 60;
 	const int ZERO_POINT = 0;
 	
--- SimilarityCheckerKATA/similaritychecker_test.cpp
+++ SimilarityCheckerKATA/similaritychecker_test.cpp
@@ -10,6 +10,10 @@ TEST(LengthCheck, Zero) {
 	LengthChecker sc;
 	EXPECT_EQ(0, sc.GetLengthPoint("A", "BB"));
 }
+TEST(LengthCheck, PartialPoint) {
+	LengthChecker sc;
+	EXPECT_EQ(20, sc.GetLengthPoint("AAABB", "BAA"));
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
