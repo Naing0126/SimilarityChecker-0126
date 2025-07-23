2f1109e7ce5e8d259fbcdc623940e8ab6ec21070
naing.yoon <naing.yoon@samsung.com>
2025-07-23 15:35:27 +0900

    [Refactoring]

--- SimilarityCheckerKATA/similaritychecker.cpp
+++ SimilarityCheckerKATA/similaritychecker.cpp
@@ -6,9 +6,16 @@ public:
 	int GetLengthPoint(string str1, string str2) {
 		if (str1.length() == str2.length()) return MAX_POINT;
 		
-		if (str1.length() >= str2.length() * 2) return 0;
-		if (str1.length() * 2 <= str2.length()) return 0;
+		if (IsBiggerThanTwice(str1, str2)) return ZERO_POINT;
 	}
 private:
+	bool IsBiggerThanTwice(std::string& str1, std::string& str2)
+	{
+		if (str1.length() >= str2.length() * 2) return true;
+		if (str2.length() >= str1.length() * 2) return true;
+		return false;
+	}
 	const int MAX_POINT = 60;
+	const int ZERO_POINT = 0;
+	
 };
